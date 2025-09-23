e0bc5c773e5a7c42c1179c60cedc187300c66c08
tahoito <taho.ito0304@gmail.com>
2025-09-23 22:40:01 +0900

    perfect

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.baseURL = import.meta.env.VITE_APP_URL ?? window.location.origin;
+
+console.log("VITE_APP_URL", import.meta.env.VITE_APP_URL);
+console.log("window.location.origin", window.location.origin);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
@@ -9,5 +10,32 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Tastie',
+                short_name: 'Tastie',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#f59e0b',
+                icons: [
+                    {
+                        src: '/icons/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icons/icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
     ],
+    server: {
+        https: false, // dev時だけ
+    },
+    base: process.env.ASSET_URL ? process.env.ASSET_URL + '/' : '/build/',
 });
