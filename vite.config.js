12670ce6667ce53a5ce930a19da54efbd1dd08f3
tahoito <taho.ito0304@gmail.com>
2025-10-30 12:56:21 +0900

    icon

--- vite.config.js
+++ vite.config.js
@@ -34,8 +34,8 @@ export default defineConfig({
         background_color: '#ffffff',
         theme_color: '#F59E0B',
         icons: [
-          { src: '/images/recipe_icon.png', sizes: '192x192', type: 'image/png' },
-          { src: '/images/recipe_icon.png', sizes: '512x512', type: 'image/png' },
+          { src: '/images/pureat_icon.png', sizes: '192x192', type: 'image/png' },
+          { src: '/images/pureat_icon.png', sizes: '512x512', type: 'image/png' },
         ],
       },
       workbox: {
